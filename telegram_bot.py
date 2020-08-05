4bd8e60cbf20a508fccbac51edafc1d797274949
kunoada <kuno.ada.pool@gmail.com>
2020-08-05 19:27:18 +0200

    pool change get ticker if any subscribers

--- telegram_bot.py
+++ telegram_bot.py
@@ -740,7 +740,6 @@ def handle_battle(data):
 
 def handle_wallet_poolchange(data):
     pool_id = data['pool']
-    ticker = db.get_ticker_from_pool_id(pool_id)[0]
     chat_ids = db.get_chat_ids_from_pool_id(pool_id)
     if 'ticker' in data['change']:
         new_ticker = data['change']['ticker']['new_value']
@@ -765,6 +764,7 @@ def handle_wallet_poolchange(data):
                   f"From: {set_prefix(round(int(data['change']['pledge']['old_value']) / 1000000))} {ada}\n" \
                   f"To: {set_prefix(round(int(data['change']['pledge']['new_value']) / 1000000))} {ada}"
     for chat_id in chat_ids:
+        ticker = db.get_ticker_from_pool_id(pool_id)[0]
         send_message(message, chat_id)
         message_type = db.get_option(chat_id, ticker, 'pool_change')
         if message_type:
